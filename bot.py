4056eed9e68211298398e8a1f20d421831fce9a8
HuyNguyenAu <huynguyenau@gmail.com>
2021-04-21 21:48:21 +1000

    Working news.

--- bot.py
+++ bot.py
@@ -5,7 +5,10 @@ from json import dump, load
 from os import path, makedirs
 from getpass import getpass
 
-from nio import AsyncClient, AsyncClientConfig, LoginResponse
+from nio import AsyncClient, AsyncClientConfig, LoginResponse, RoomMessageText, SyncResponse
+
+from urllib import request
+from lxml import html
 
 
 async def send_message(content: dict, room_id: str, client: AsyncClient) -> None:
@@ -39,7 +42,7 @@ async def get_joined_rooms(client: AsyncClient) -> str:
     return joined_rooms.rooms
 
 
-async def get_credentials(user_id: str, device_id: str, home_server: str, store_path: str):
+async def get_credentials(user_id: str, device_id: str, home_server: str, store_path: str) -> LoginResponse:
     client_config = AsyncClientConfig(
         max_limit_exceeded=0,
         max_timeouts=0,
@@ -96,14 +99,14 @@ def get_client(user_id: str, device_id: str, home_server: str, access_token: str
     return client
 
 
-def get_bot_config(path: str):
+def get_bot_config(path: str) -> str:
     if does_file_exists(path):
         with open(path, "r") as file:
             return load(file)
     return None
 
 
-def load_credentials(path: str):
+def load_credentials(path: str) -> str:
     if does_file_exists(path):
         with open(path, "r") as file:
             return load(file)
@@ -121,6 +124,7 @@ async def main() -> None:
     store_path = "store"
     bot_path = "bot.json"
     bot_config = get_bot_config(bot_path)
+    news_path = "news.txt"
 
     if not path.exists(store_path):
         makedirs(store_path)
@@ -152,26 +156,112 @@ async def main() -> None:
         store_path
     )
 
+    # await leave_room(bot_config["testing_room"], client)
     await join_room(bot_config["testing_room"], client)
 
     if client.should_upload_keys:
         await client.keys_upload()
 
-    await client.sync(timeout=30000, full_state=True)
+    sync = await client.sync(timeout=30000, full_state=True)
+    links = await get_room_links(bot_config["testing_room"], sync, client)
+    y_news = get_y_news(links)
 
-    await send_message(
-        {
-            "msgtype": "m.text",
-            "body": "Google\nhttps://www.google.com.au\n\nTest\ntest.com"
-        },
-        bot_config["testing_room"],
-        client
-    )
+    if y_news is not None:
+        await send_message(
+            {
+                "msgtype": "m.text",
+                "body": y_news
+            },
+            bot_config["testing_room"],
+            client
+        )
+
+    links = await get_room_links(bot_config["testing_room"], sync, client)
+    the_hacker_news = get_the_hacker_news(links)
+
+    if the_hacker_news is not None:
+        await send_message(
+            {
+                "msgtype": "m.text",
+                "body": the_hacker_news
+            },
+            bot_config["testing_room"],
+            client
+        )
 
     await client.close()
+
+
+async def get_room_links(room_id: str, sync: SyncResponse, client: AsyncClient) -> list:
+    links = []
+    start = sync.next_batch
+
+    while True:
+        messages = await client.room_messages(room_id, start)
+
+        if start == messages.end:
+            break
+
+        start = messages.end
+
+        for text in messages.chunk:
+            if isinstance(text, RoomMessageText):
+                for line in str(text).split('\n'):
+                    if line.startswith("https://"):
+                        links.append(line)
+
+    return links
+
+
+def get_the_hacker_news(existing_links: list) -> str:
+    news = []
+    resource = request.urlopen("https://thehackernews.com/")
+    page = html.fromstring(resource.read().decode(
+        resource.headers.get_content_charset()))
+
+    for link in page.xpath("//a[@class='story-link']"):
+        title = link.xpath(".//h2[@class='home-title']")[0].text
+        link = link.get("href")
+
+        if link in existing_links:
+            continue
+
+        news.append(f"{title}\n{link}")
+
+    if len(news) <= 0:
+        return None
+
+    news.insert(0, "The Hacker News")
+    news.append("\n")
+    return "\n\n".join(news)
+
+
+def get_y_news(existing_links: list) -> str:
+    news = []
+    resource = request.urlopen("https://news.ycombinator.com/")
+    page = html.fromstring(resource.read().decode(
+        resource.headers.get_content_charset()))
+
+    for link in page.xpath("//a[@class='storylink']"):
+        title = link.text
+        link = link.get("href")
+
+        if link in existing_links:
+            continue
+
+        news.append(f"{title}\n{link}")
+
+    if len(news) <= 0:
+        return None
+
+    news.insert(0, "Hacker News")
+    news.append("\n")
+    return "\n\n".join(news)
+
 
 if __name__ == "__main__":
     try:
         run(main())
+
     except KeyboardInterrupt:
         pass
