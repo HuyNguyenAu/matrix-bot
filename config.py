c8e2aebc30da413293583e034485fbab03d33a4f
HuyNguyenAu <huynguyenau@gmail.com>
2021-04-22 18:26:23 +1000

    Updated.

---
+++ config.py
@@ -0,0 +1,63 @@
+import json
+
+
+class Config:
+    def __init__(self, path):
+        config = self.__load_config(path)
+
+        self.__user_id = config["bot"]["user_id"]
+        self.__device_name = config["bot"]["device_name"]
+        self.__device_id = config["bot"]["device_id"]
+        self.__home_server = config["bot"]["home_server"]
+        self.__access_token = config["bot"]["access_token"]
+        self.__rooms = config["bot"]["rooms"]
+        self.__store_path = config["bot"]["store_path"]
+        self.__news = config["news"]
+
+    def __load_config(self, path: str) -> dict:
+        with open(path, "r") as file:
+            return json.load(file)
+
+    def get_user_id(self) -> str:
+        return self.__user_id
+
+    def get_device_name(self) -> str:
+        return self.__device_name
+
+    def get_device_id(self) -> str:
+        return self.__device_id
+
+    def set_device_id(self, device_id: str) -> str:
+        self.__device_id = device_id
+
+    def get_home_server(self) -> str:
+        return self.__home_server
+
+    def get_access_token(self) -> str:
+        return self.__access_token
+
+    def set_access_token(self, access_token: str) -> None:
+        self.__access_token = access_token
+
+    def get_rooms(self) -> dict:
+        return self.__rooms
+
+    def get_store_path(self) -> str:
+        return self.__store_path
+
+    def save_config(self) -> str:
+        with open("test.json", "w") as file:
+            return json.dump(
+                obj={
+                    "bot": {
+                        "user_id": self.__user_id,
+                        "device_name": self.__device_name,
+                        "device_id": self.__device_id,
+                        "home_server": self.__home_server,
+                        "access_token": self.__access_token,
+                        "store_path": self.__store_path,
+                        "rooms": self.__rooms
+                    },
+                    "news": self.__news
+                },
+                fp=file)
---
+++ login.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+import getpass
+import os
+import nio
+import asyncio
+import traceback
+
+from config import Config
+
+
+async def main() -> None:
+    config_path = "config.json"
+    config = Config(config_path)
+    store_path = config.get_store_path()
+
+    if not os.path.exists(store_path):
+        os.makedirs(store_path)
+
+    if len(config.get_access_token()) > 0:
+        print("Access token already set.")
+        sys.exit(1)
+
+    client_config = nio.AsyncClientConfig(
+        max_limit_exceeded=0,
+        max_timeouts=0,
+        store_sync_tokens=True,
+        encryption_enabled=True,
+    )
+
+    client = nio.AsyncClient(
+        homeserver=config.get_home_server(),
+        user=config.get_user_id(),
+        store_path=config.get_store_path(),
+        config=client_config,
+        ssl=True
+    )
+
+    response = await client.login(
+        password=getpass.getpass(),
+        device_name=config.get_device_name()
+    )
+    await client.close()
+
+    if not isinstance(response, nio.LoginResponse):
+        print("Failed to get access token.")
+        sys.exit(1)
+
+    config.set_access_token(response.access_token)
+    config.set_device_id(response.device_id)
+    config.save_config()
+
+if __name__ == "__main__":
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except Exception:
+        print(traceback.format_exc())
+        sys.exit(1)
+    except KeyboardInterrupt:
+        sys.exit(0)
