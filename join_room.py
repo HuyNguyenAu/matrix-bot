598ebb95fa0285fe14e5291c28dcf3437dc06363
HuyNguyenAu <huynguyenau@gmail.com>
2021-04-23 16:34:24 +1000

    Added config.

--- join_room.py
+++ join_room.py
@@ -2,10 +2,11 @@
 
 import sys
 
+from config import Config
 from bot import Bot
 
 if __name__ == "__main__":
     room_id = sys.argv[1]
-    bot = Bot()
+    bot = Bot(Config())
     bot.join_room(room_id)
     bot.close_client()
